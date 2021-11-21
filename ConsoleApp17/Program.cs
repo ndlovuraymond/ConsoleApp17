80aef2c42daa8e87008494cdd7d9bf228e11dbd9
ndlovuraymond <93596122+ndlovuraymond@users.noreply.github.com>
2021-11-21 23:36:43 +0200

    Add project files.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,338 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to tic-tac-toe! \n" + "   |   | \n" +
+                "---+---+--- \n" + "   |   |  \n" +
+                "---+---+--- \n" + "   |   |");
+            string a;
+            string b;
+            string c;
+            string d;
+            string e;
+            string f;
+            string g;
+            string h;
+            string i;
+            int turn = 1;
+            string choice;
+            while
+                (turn <= 9)
+                turn++;
+                if (turn % 2 == 0) ;
+                Console.Write("X’s move >  ");
+                choice = (Console.ReadLine());
+            if (choice == "0")
+                Console.Write("Illegal move!Try Again!");
+                if (choice == "1")
+                    a = "X";
+                else a = " ";
+                if (choice == "2")
+                    b = "X";
+                else b = " ";
+                if (choice == "3")
+                    c = "X";
+                else c = " ";
+                if (choice == "4")
+                    d = "X";
+                else d = " ";
+                if (choice == "5")
+                    e = "X";
+                else e = " ";
+                if (choice == "6")
+                    f = "X";
+                else f = " ";
+                if (choice == "7")
+                    g = "X";
+                else g = " ";
+                if (choice == "8")
+                    h = "X";
+                else h = " ";
+                if (choice == "9")
+                    i = "X";
+                else i = " ";
+
+                Console.WriteLine($" {a} | {b} | {c} \n" + "---+---+--- \n" + $" {d} | {e} | {f} \n" + "---+---+--- \n"
+                    + $" {g} | {h} | {i} \n");
+                if (turn % 2 == 1) ;
+                Console.Write("O’s move > ");
+                choice = (Console.ReadLine());
+            if (choice == "0")
+                Console.Write("Illegal move!Try Again!");
+            if (choice == "1")
+                    a = "O";
+                else a = " ";
+                if (choice == "2")
+                    b = "O";
+                else b = " ";
+                if (choice == "3")
+                    c = "O";
+                else c = " ";
+                if (choice == "4")
+                    d = "O";
+                else d = " ";
+                if (choice == "5")
+                    e = "O";
+                else e = " ";
+                if (choice == "6")
+                    f = "O";
+                else f = " ";
+                if (choice == "7")
+                    g = "O";
+                else g = " ";
+                if (choice == "8")
+                    h = "O";
+                else h = " ";
+                if (choice == "9")
+                    i = "O";
+                else i = " ";
+                Console.WriteLine($" {a} | {b} | {c} \n" + "---+---+--- \n" + $" {d} | {e} | {f} \n" + "---+---+--- \n"
+                   + $" {g} | {h} | {i} \n");
+                if (turn % 2 == 0) ;
+                Console.Write("X’s move >  ");
+                choice = (Console.ReadLine());
+            if (choice == "0")
+                Console.Write("Illegal move!Try Again!");
+            if (choice == "1")
+                    a = "X";
+                else a = " ";
+                if (choice == "2")
+                    b = "X";
+                else b = " ";
+                if (choice == "3")
+                    c = "X";
+                else c = " ";
+                if (choice == "4")
+                    d = "X";
+                else d = " ";
+                if (choice == "5")
+                    e = "X";
+                else e = " ";
+                if (choice == "6")
+                    f = "X";
+                else f = " ";
+                if (choice == "7")
+                    g = "X";
+                else g = " ";
+                if (choice == "8")
+                    h = "X";
+                else h = " ";
+                if (choice == "9")
+                    i = "X";
+                else i = " ";
+                Console.WriteLine($" {a} | {b} | {c} \n" + "---+---+--- \n" + $" {d} | {e} | {f} \n" + "---+---+--- \n"
+                    + $" {g} | {h} | {i} \n");
+                if (turn % 2 == 1) ;
+                Console.Write("O’s move > ");
+                choice = (Console.ReadLine());
+            if (choice == "0")
+                Console.Write("Illegal move!Try Again!");
+            if (choice == "1")
+                    a = "O";
+                else a = " ";
+                if (choice == "2")
+                    b = "O";
+                else b = " ";
+                if (choice == "3")
+                    c = "O";
+                else c = " ";
+                if (choice == "4")
+                    d = "O";
+                else d = " ";
+                if (choice == "5")
+                    e = "O";
+                else e = " ";
+                if (choice == "6")
+                    f = "O";
+                else f = " ";
+                if (choice == "7")
+                    g = "O";
+                else g = " ";
+                if (choice == "8")
+                    h = "O";
+                else h = " ";
+                if (choice == "9")
+                    i = "O";
+                else i = " ";
+            Console.WriteLine($" {a} | {b} | {c} \n" + "---+---+--- \n" + $" {d} | {e} | {f} \n" + "---+---+--- \n"
+            + $" {g} | {h} | {i} \n");
+            if (turn % 2 == 0) ;
+            Console.Write("X's move > ");
+            choice = (Console.ReadLine());
+            if (choice == "0")
+                Console.Write("Illegal move!Try Again!");
+            if (choice == "1")
+                a = "X";
+            else a = " ";
+            if (choice == "2")
+                b = "X";
+            else b = " ";
+            if (choice == "3")
+                c = "X";
+            else c = " ";
+            if (choice == "4")
+                d = "X";
+            else d = " ";
+            if (choice == "5")
+                e = "X";
+            else e = " ";
+            if (choice == "6")
+                f = "X";
+            else f = " ";
+            if (choice == "7")
+                g = "X";
+            else g = " ";
+            if (choice == "8")
+                h = "X";
+            else h = " ";
+            if (choice == "9")
+                i = "X";
+            else i = " ";
+            Console.WriteLine($" {a} | {b} | {c} \n" + "---+---+--- \n" + $" {d} | {e} | {f} \n" + "---+---+--- \n"
+                + $" {g} | {h} | {i} \n");
+            if (turn % 2 == 1) ;
+            Console.Write("O’s move > ");
+            choice = (Console.ReadLine());
+            if (choice == "0")
+                Console.Write("Illegal move!Try Again!");
+            if (choice == "1")
+                a = "O";
+            else a = " ";
+            if (choice == "2")
+                b = "O";
+            else b = " ";
+            if (choice == "3")
+                c = "O";
+            else c = " ";
+            if (choice == "4")
+                d = "O";
+            else d = " ";
+            if (choice == "5")
+                e = "O";
+            else e = " ";
+            if (choice == "6")
+                f = "O";
+            else f = " ";
+            if (choice == "7")
+                g = "O";
+            else g = " ";
+            if (choice == "8")
+                h = "O";
+            else h = " ";
+            if (choice == "9")
+                i = "O";
+            else i = " ";
+            Console.WriteLine($" {a} | {b} | {c} \n" + "---+---+--- \n" + $" {d} | {e} | {f} \n" + "---+---+--- \n"
+                + $" {g} | {h} | {i} \n");
+            if (turn % 2 == 0) ;
+            Console.Write("X’s move >  ");
+            choice = (Console.ReadLine());
+            if (choice == "0")
+                Console.Write("Illegal move!Try Again!");
+            if (choice == "1")
+                a = "X";
+            else a = " ";
+            if (choice == "2")
+                b = "X";
+            else b = " ";
+            if (choice == "3")
+                c = "X";
+            else c = " ";
+            if (choice == "4")
+                d = "X";
+            else d = " ";
+            if (choice == "5")
+                e = "X";
+            else e = " ";
+            if (choice == "6")
+                f = "X";
+            else f = " ";
+            if (choice == "7")
+                g = "X";
+            else g = " ";
+            if (choice == "8")
+                h = "X";
+            else h = " ";
+            if (choice == "9")
+                i = "X";
+            else i = " ";
+            Console.WriteLine($" {a} | {b} | {c} \n" + "---+---+--- \n" + $" {d} | {e} | {f} \n" + "---+---+--- \n"
+                + $" {g} | {h} | {i} \n");
+            if (turn % 2 == 1) ;
+            Console.Write("O’s move > ");
+            choice = (Console.ReadLine());
+            if (choice == "0")
+                Console.Write("Illegal move!Try Again!");
+            if (choice == "1")
+                a = "O";
+            else a = " ";
+            if (choice == "2")
+                b = "O";
+            else b = " ";
+            if (choice == "3")
+                c = "O";
+            else c = " ";
+            if (choice == "4")
+                d = "O";
+            else d = " ";
+            if (choice == "5")
+                e = "O";
+            else e = " ";
+            if (choice == "6")
+                f = "O";
+            else f = " ";
+            if (choice == "7")
+                g = "O";
+            else g = " ";
+            if (choice == "8")
+                h = "O";
+            else h = " ";
+            if (choice == "9")
+                i = "O";
+            else i = " ";
+            Console.WriteLine($" {a} | {b} | {c} \n" + "---+---+--- \n" + $" {d} | {e} | {f} \n" + "---+---+--- \n"
+                + $" {g} | {h} | {i} \n");
+            if (turn % 2 == 0) ;
+            Console.Write("X’s move >  ");
+            choice = (Console.ReadLine());
+            if (choice == "0")
+                Console.Write("Illegal move!Try Again!");
+            if (choice == "1")
+                a = "X";
+            else a = " ";
+            if (choice == "2")
+                b = "X";
+            else b = " ";
+            if (choice == "3")
+                c = "X";
+            else c = " ";
+            if (choice == "4")
+                d = "X";
+            else d = " ";
+            if (choice == "5")
+                e = "X";
+            else e = " ";
+            if (choice == "6")
+                f = "X";
+            else f = " ";
+            if (choice == "7")
+                g = "X";
+            else g = " ";
+            if (choice == "8")
+                h = "X";
+            else h = " ";
+            if (choice == "9")
+                i = "X";
+            else i = " ";
+            Console.WriteLine($" {a} | {b} | {c} \n" + "---+---+--- \n" + $" {d} | {e} | {f} \n" + "---+---+--- \n"
+                + $" {g} | {h} | {i} \n");
+            Console.Write("Game over!");
+            Console.ReadKey();
+
+        }
+    }
+}
